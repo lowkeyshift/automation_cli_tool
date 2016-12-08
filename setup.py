859e2b1d902e3e0d284e8d71d56c7a3841f17fde
ecarrasquillo <ecarrasquillo@Random.local>
2016-12-07 22:46:13 -0500

    added the setup.cfg and setup.py file

---
+++ setup.py
@@ -0,0 +1,64 @@
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+
+from skele import __version__
+
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()	
+class RunTests(Command):
+	"""Run all the Tests."""
+	description = 'run tests'
+	user_option = []
+
+	def initialize_options(self):
+		pass
+
+	def finalize_option(self):
+		pass
+
+	def run(self):
+		"""Run all tests!"""
+		errno = call(['py.test', '--cov=skele', '--cov-report=term-missing'])
+        raise SystemExit(errno)
+
+setup(
+	    name = 'auto',
+    version = __version__,
+    description = 'Puppet Infrastructure command line program in Python.',
+    long_description = long_description,
+    url = 'https://github.com/lowkeyshift/automation_cli_tool',
+    author = 'Edwin Carrasquillo',
+    author_email = 'ecarra7979@gmail.com',
+    license = 'UNLICENSE',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = 'cli',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'auto=auto.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
